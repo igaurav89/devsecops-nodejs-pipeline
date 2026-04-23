20c8f3d3bc6caa71d4de60019e3eab62fcb96159
Gaurav <gauravdesale15@gmail.com>
2026-04-23 11:05:10 +0530

    Fixed app export for tests

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('DevSecOps Pipeline Running');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+module.exports = app;
+
+// Start server only when run directly
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+}
