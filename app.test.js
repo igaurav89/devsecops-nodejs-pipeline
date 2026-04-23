b57ae79d14d41387234626249c04d8af931a127f
Gaurav <gauravdesale15@gmail.com>
2026-04-23 12:58:45 +0530

    Updated test for new response

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   test('should return success message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('DevSecOps Pipeline Running');
+    expect(res.text).toBe('DevSecOps Pipeline Running and my name is gaurav desale');
   });
 });
